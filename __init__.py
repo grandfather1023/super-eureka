0ff8bd59476cb12d81df8cbef96a868d8a07d355
grandfather1023 <grandfather1023@gmail.com>
2025-01-08 00:05:51 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Copyright (c) 2016 Colin Duquesnoy (QCrash project)
-# Copyright (c) 2018- Spyder Project Contributors
+# Copyright (c) 2009- Spyder Project Contributors
 #
-# Licensed under the terms of the MIT License
-# (see LICENSE.txt in this directory for details)
+# Distributed under the terms of the MIT License
+# (see spyder/__init__.py for details)
 # -----------------------------------------------------------------------------
 
-"""
-spyder.widgets.github
-=====================
 
-Widgets defined in this module were adapted from the
-`QCrash Project <https://github.com/ColinDuquesnoy/QCrash>`_.
+"""
+Introspection utilities used by Spyder.
 """
